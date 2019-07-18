c53362a26449b2f69a8faea4cd82faa8f7eb3d6f
mohonmia <mohonmia97@gmail.com>
2019-07-18 19:48:20 +0600

    agin added

--- C-program-to-enter-two-numbers-and-perform-all-arithmetic-operations.c
+++ C-program-to-enter-two-numbers-and-perform-all-arithmetic-operations.c
@@ -2,13 +2,5 @@
 
 int main(){
     int sum, sub, multi, divide,m,n,result;
-    scanf("%d %d",&m,&n);
-    sum = m + n;
-    printf("%d \n",sum);
-    sub = m - n;
-    printf("%d \n",sub);
-    multi = m * n;
-    printf("%d \n",multi);
-    divide = m % n;
-    printf("%d \n",divide);
+    printf("HELLO WORLD");
 }
